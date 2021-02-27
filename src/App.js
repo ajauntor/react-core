84ce6efb4ecfa76b9d639de81387fcd80c9c9ee5
AJ Auntor <auntorknocks@gmail.com>
2021-02-27 13:15:34 +0600

    Pass Object and read Object

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-const programmer = ["Jhankar Mahbub", "Hanry costal", "Luice Zar"]
+  const productList=[
+    {names:"Hero Hunk", ccLimit:"160kmp", price:"$3216"},
+    {names:"Pulsar NS", ccLimit:"175kmp", price:"$3010"}
+]
   return (
     <div className="App">
       <header className="App-header">
-      <Mango name={programmer[0]} job="Web Developer"></Mango>
-      <Mango name={programmer[2]} job="Super Bike Rider"></Mango>
-      <Mango name={programmer[1]}></Mango>
+        <Product></Product>
+      <Product heroHunk={productList[0]}></Product>
       </header>
     </div>
   );
 }
-function Mango(props){
-  const mangoStyle ={
-    border:"1px solid green",
-    margin: "10px",
+function Product(props){
+  const productStyle={
+    backgroundColor: "#524444",
+    border:"10px solid #3D2F2F",
+    borderRadius: "5px",
+    width:"400px",
+    height:"370px",
+    float:"left",
   }
-  return (
-  <div style={mangoStyle}>
-    <h2>Name: {props.name}</h2>
-    <h4>Job: {props.job}</h4>
-  </div>
-  );
+  console.log(props);
+  return(
+    <div style={productStyle}>
+      <h3>Bike Model:{props.heroHunk.names}</h3>
+      <h2>Max Speed:{props.speed}</h2>
+      <button>Buy Now</button>
+    </div>
+  )
 }
-
 export default App;
