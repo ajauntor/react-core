252a91ed5ae0ddc8daa8528432dc0c4aa62dcff6
AJ Auntor <auntorknocks@gmail.com>
2021-02-27 19:25:39 +0600

    Add Dynamic User Data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Counter></Counter>
+      <Users></Users>
       </header>
     </div>
   );
@@ -18,6 +19,18 @@ function Counter (){
       <h3>Count:{count}</h3>
       <button onClick={ () => setCount (count + 1)}>Increase</button>
       <button onClick={ () => setCount(count - 1)}>Decrease</button>
+    </div>
+  )
+}
+function Users (){
+  const [user, setUser]= useState([]);
+  useEffect(() => {
+    
+  }
+  )
+  return (
+    <div>
+      <h5>All of Dynamic User</h5>
     </div>
   )
 }
