980eb3de7389184b0e60a9abec3c5c48b17a38e3
AJ Auntor <auntorknocks@gmail.com>
2021-02-27 10:47:19 +0600

    Pass Dynamic data in componants

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+const programmer = ["Jhankar Mahbub", "Hanry costal", "Luice Zar"]
   return (
     <div className="App">
       <header className="App-header">
-      <Mango></Mango>
-      <Mango></Mango>
+      <Mango name={programmer[0]} job="Web Developer"></Mango>
+      <Mango name={programmer[2]} job="Super Bike Rider"></Mango>
+      <Mango name={programmer[1]}></Mango>
       </header>
     </div>
   );
 }
-function Mango(){
+function Mango(props){
   const mangoStyle ={
     border:"1px solid green",
     margin: "10px",
   }
   return (
   <div style={mangoStyle}>
-    <h2 style={{padding:"0 5px"}}>Mango is very famous food in bangladesh</h2>
-    <h4>So, let's eat mango!</h4>
+    <h2>Name: {props.name}</h2>
+    <h4>Job: {props.job}</h4>
   </div>
   );
 }
